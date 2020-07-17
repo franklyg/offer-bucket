e2b2c9d709966a17e27fe50ed317c0f3670e2d15
franklyg <f.gisonni@gmail.com>
2020-07-17 16:29:50 -0400

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import Container from '../components/container'
-// import MoreStories from '../components/more-stories'
-// import HeroPost from '../components/hero-post'
-// import Intro from '../components/intro'
-// import Layout from '../components/layout'
-// import { getAllPostsForHome } from '../lib/api'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import { getAllPostsForHome } from '../lib/api'
 import Head from 'next/head'
 import { CMS_NAME } from '../lib/constants'
 
 export default function Index({ preview, allPosts }) {
-  // const heroPost = allPosts[0].node
-  // const morePosts = allPosts.slice(1)
+  const heroPost = allPosts[0].node
+  const morePosts = allPosts.slice(1)
   return (
     <>
       <Layout>
@@ -31,9 +31,9 @@ export default function Index({ preview, allPosts }) {
   )
 }
 
-// export async function getStaticProps({ preview = false, previewData }) {
-//   const allPosts = await getAllPostsForHome(previewData)
-//   return {
-//     props: { preview, allPosts },
-//   }
-// }
+export async function getStaticProps({ preview = false, previewData }) {
+  const allPosts = await getAllPostsForHome(previewData)
+  return {
+    props: { preview, allPosts },
+  }
+}
